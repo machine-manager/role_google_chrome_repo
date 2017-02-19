8e854dc1245a86d5e34ec16c3c7c6bcbc5618462
Ivan Kozik <ivan@ludios.org>
2017-02-19 14:12:10 +0000

    Add comment

--- lib/role_wine.ex
+++ lib/role_wine.ex
@@ -8,7 +8,7 @@ defmodule RoleWine do
 		wine_packages = [
 			"wine-staging",
 			"winehq-staging", # to set up wine-staging as the default wine (/usr/bin/wine)
-			"winetricks",
+			"winetricks",     # note we have a newer version in custom-packages
 		]
 		%{
 			desired_packages: wine_packages,
