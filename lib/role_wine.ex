bdb2b1d39c5b8b24586786b5aef02a362fb130da
Ivan Kozik <ivan@ludios.org>
2017-02-19 14:04:39 +0000

    wine role

---
+++ lib/role_wine.ex
@@ -0,0 +1,19 @@
+alias Converge.Util
+
+defmodule RoleWine do
+	require Util
+	Util.declare_external_resources("files")
+
+	def role(_tags \\ []) do
+		wine_packages = [
+			"wine-staging",
+			"winehq-staging", # to set up wine-staging as the default wine (/usr/bin/wine)
+			"winetricks",
+		]
+		%{
+			desired_packages: wine_packages,
+			apt_keys:         [content("files/apt_keys/77C899CB Launchpad PPA for Wine.txt")],
+			apt_sources:      ["deb http://ppa.launchpad.net/wine/wine-builds/ubuntu xenial main"],
+		}
+	end
+end
