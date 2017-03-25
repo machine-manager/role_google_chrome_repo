a05e7cc2163d52f048e9d6a4746309e411986743
Ivan Kozik <ivan@ludios.org>
2017-03-25 08:22:07 +0000

    Fix a converge failure when a chrome is being purged

--- lib/role.ex
+++ lib/role.ex
@@ -7,10 +7,8 @@ defmodule RoleGoogleChromeRepo do
 	def role(_tags \\ []) do
 		# We don't install any particular Chrome package here because there are
 		# three to choose from: google-chrome-{stable,beta,unstable}
-		%{
-			apt_keys:         [Util.content("files/apt_keys/D38B4796 Google Inc. (Linux Packages Signing Authority).txt")],
-			apt_sources:      ["deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"],
-			pre_install_unit: %All{units: [
+		etc_default_files =
+			%All{units: [
 				# Google Chrome installs symlinks at /etc/cron.daily/google-chrome*;
 				# these scripts function as a little configuration manager that re-adds
 				# apt keys and apt sources if they are missing (e.g. after an Ubuntu
@@ -23,7 +21,11 @@ defmodule RoleGoogleChromeRepo do
 				%FilePresent{path: "/etc/default/google-chrome",          content: "exit 0\n", mode: 0o644},
 				%FilePresent{path: "/etc/default/google-chrome-beta",     content: "exit 0\n", mode: 0o644},
 				%FilePresent{path: "/etc/default/google-chrome-unstable", content: "exit 0\n", mode: 0o644},
-			]},
+			]}
+		%{
+			apt_keys:         [Util.content("files/apt_keys/D38B4796 Google Inc. (Linux Packages Signing Authority).txt")],
+			apt_sources:      ["deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"],
+			pre_install_unit: etc_default_files,
 			post_install_unit: %All{units: [
 				# The scripts in /etc/cron.daily/ are already no-op'ed by the /etc/default/google-chrome-*
 				# files, but delete them anyway because we don't need them.  Note that
@@ -32,6 +34,11 @@ defmodule RoleGoogleChromeRepo do
 				%FileMissing{path: "/etc/cron.daily/google-chrome"},
 				%FileMissing{path: "/etc/cron.daily/google-chrome-beta"},
 				%FileMissing{path: "/etc/cron.daily/google-chrome-unstable"},
+
+				# Do this in post_install_unit as well because if e.g. google-chrome-unstable
+				# is being purged, apt will remove /etc/cron.daily/google-chrome-unstable and
+				# cause a converge failure.
+				etc_default_files,
 			]},
 		}
 	end
